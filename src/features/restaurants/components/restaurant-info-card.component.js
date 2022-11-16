fc2f214086b41d978527bf7579cd6c98207c92a5
Joran-Vergauwen <joran.vergauwen@gmail.com>
2022-11-16 05:56:12 +0100

    Until lesson 160

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { SvgXml } from "react-native-svg";
+
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import {
+  Address,
+  Icon,
+  Info,
+  Rating,
+  RestaurantCard,
+  RestaurantCardCover,
+  Section,
+  SectionEnd,
+} from "./restaurant-info-card.styles";
+
+const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://cdn.vox-cdn.com/thumbor/OheW0CNYdNihux9eVpJ958_bVCE=/0x0:5996x4003/1200x900/filters:focal(1003x1633:1961x2591)/cdn.vox-cdn.com/uploads/chorus_image/image/51830567/2021_03_23_Merois_008.30.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+    placeId,
+  } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map((_, i) => (
+              <SvgXml
+                key={`star-${placeId}-${i}`}
+                xml={star}
+                width={20}
+                height={20}
+              />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
+
+export default RestaurantInfoCard;
---
+++ src/services/restaurants/mock/index.js
@@ -0,0 +1,33 @@
+import antwerp from "./antwerp.json";
+import chicago from "./chicago.json";
+import toronto from "./toronto.json";
+import san_francisco from "./san_francisco.json";
+
+export const mocks = {
+  "51.219448,4.402464": antwerp,
+  "43.653225,-79.383186": toronto,
+  "41.878113,-87.629799": chicago,
+  "37.7749295,-122.4194155": san_francisco,
+};
+
+export const mockImages = [
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-oranges-ice-600x750.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-baking-600x750.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-pancakes-600x750.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/messy-pizza-on-a-black-table-600x400.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/pizza-ready-for-baking-600x400.jpg",
+  "https://www.florisrestaurant.be/imager/defaultassets/417/FLORIS_LR_161A9402_4f5297411002b892fc2ab34d34ecd587.jpg",
+  "https://www.linguahouse.com/linguafiles/md5/d01dfa8621f83289155a3be0970fb0cb",
+  "https://www.desmaakvanitalie.nl/wp-content/uploads/2022/03/pizza-margherita-napels-unsplash.jpg",
+  "https://st.depositphotos.com/1518767/4293/i/600/depositphotos_42930411-stock-photo-concentrated-male-chef-garnishing-food.jpg",
+  "https://images.unsplash.com/photo-1572116469696-31de0f17cc34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29ja3RhaWwlMjBiYXJ8ZW58MHx8MHx8&w=1000&q=80",
+  "https://images0.persgroep.net/rcs/fu-2nKDrAsI1mMEAQ77xkS3tTiw/diocontent/109395483/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
+  "https://media-cdn.tripadvisor.com/media/photo-s/10/e3/02/5b/table-setting.jpg",
+  "https://images0.persgroep.net/rcs/51WxQjLMTN5OM6ORqyLaQIXfZyU/diocontent/204640194/_fill/1200/630/?appId=21791a8992982cd8da851550a453bd7f&quality=0.7",
+  "https://media-cdn.tripadvisor.com/media/photo-s/0c/49/2f/58/ter-burchgraeve.jpg",
+  "https://www.thebarnmerendree.be/images/foto05.jpg?crc=4030618485",
+  "https://notafoodexpert.com/wp-content/uploads/2018/05/Kin-Khao-Gent-840x500@2x.jpg",
+  "https://static.nieuwsblad.be/Assets/Images_Upload/2019/03/27/c61bc402-4dee-496f-bae4-f252beff5bb4_web_scale_0.1037775_0.1037775__.jpg?maxheight=475&maxwidth=633&scale=both",
+];
