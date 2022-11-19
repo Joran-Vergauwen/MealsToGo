1c56658dae759a85b05686431937797ed058aa0b
Joran-Vergauwen <joran.vergauwen@gmail.com>
2022-11-19 15:18:02 +0100

    until lesson 200

---
+++ src/features/account/screens/login.screen.js
@@ -0,0 +1,72 @@
+import React, { useState, useContext } from "react";
+
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+  Title,
+  ErrorContainer,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { Text } from "../../../components/typography/text.component";
+import { Navigation } from "../../../infrastructure/navigation";
+
+export const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { onLogin, error } = useContext(AuthenticationContext);
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Title>Meals To Go</Title>
+      <AccountContainer>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          // keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(u) => setEmail(u)}
+        />
+        <Spacer size="large">
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            secure
+            onChangeText={(p) => setPassword(p)}
+          />
+        </Spacer>
+        {error && (
+          <Spacer size="large">
+            <ErrorContainer>
+              <Text variant="error">
+                Invalid combination of username and password
+              </Text>
+            </ErrorContainer>
+          </Spacer>
+        )}
+
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => onLogin(email, password)}
+          >
+            Log in
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
+    </AccountBackground>
+  );
+};
