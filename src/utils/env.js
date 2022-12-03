d213adebf8a7c4509dd0fc6d14747f3217e6329a
Joran-Vergauwen <joran.vergauwen@gmail.com>
2022-12-03 14:05:01 +0100

    until lesson 237

---
+++ src/utils/env.js
@@ -0,0 +1,4 @@
+const liveHost = "https://us-central1-mealstogo-5dbdd.cloudfunctions.net";
+const localHost = "http://127.0.0.1:5001/mealstogo-5dbdd/us-central1";
+export const isDevelopment = process.env.NODE_ENV === "development";
+export const host = isDevelopment ? localHost : liveHost;
